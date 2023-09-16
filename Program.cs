8bc7e4c42751d090bb6d5ef16024b751c62ca6aa
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2023-09-16 13:43:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace ConsoleApp160923
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HW/Mod__/Pract/ex01\n");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+        }
+    }
+}
